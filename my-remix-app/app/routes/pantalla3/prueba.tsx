3935ce1f1259c68cdd0f06c8f60ec5542d1aeb44
DanielLR16 <daniellr_16@hotmail.com>
2022-08-17 19:10:12 +0200

    Primera parte post

--- my-remix-app/app/routes/pantalla3/prueba.tsx
+++ my-remix-app/app/routes/pantalla3/prueba.tsx
@@ -1,32 +1,43 @@
-export default function Index() {
+import { Link, useLoaderData } from "@remix-run/react";
+
+type Post = {
+  id: number;
+  slug: string;
+  title: string;
+}
+
+export function loader () {
+  const posts: Post[] = [
+    {
+      id: 1,
+      slug: 'Hello world',
+      title: 'Hello world',
+    },
+    {
+      id: 2,
+      slug: 'second-article',
+      title: 'Prueba'
+    } 
+  ]
+  return posts
+}
+
+export default function Prueba() {
+  const posts = useLoaderData<Post[]>()
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>sadjfldsa</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            Hola
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            adsfas
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Prueba</h1>
+      {
+        posts.map(post => {
+          return (
+            <div key={post.id}>
+              <Link to={`/posts/${post.slug}`}>
+                <h3>{post.title}</h3>
+              </Link>
+            </div>
+          )
+        })
+      }
     </div>
-  );
+  )
 }
